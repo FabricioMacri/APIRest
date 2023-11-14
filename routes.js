0bb3e0fd1e1bfbb161f5d0bb77c4003953fb2621
fabriciomacri <fabriciomacri@hotmail.com>
2023-11-13 23:37:35 -0300

    cambios de tabla

--- routes.js
+++ routes.js
@@ -7,7 +7,7 @@ routes.get('/', (req, res)=>{
     req.getConnection((err, conn)=>{
         if(err) return res.send(err)
 
-        conn.query('SELECT * FROM products', (err, rows)=>{
+        conn.query('SELECT * FROM u284866064_coopedatabase.products', (err, rows)=>{
             if(err) return res.send(err)
 
             res.json(rows)
@@ -29,7 +29,7 @@ routes.get('/item/:id', (req, res)=>{
     req.getConnection((err, conn)=>{
         if(err) return res.send(err)
         
-        conn.query('SELECT * FROM `e-commerce`.products WHERE ID = ?', [req.params.id], (err, rows)=>{
+        conn.query('SELECT * FROM u284866064_coopedatabase.products WHERE ID = ?', [req.params.id], (err, rows)=>{
             if(err) return res.send(err)
 
             res.json(rows)
@@ -40,7 +40,7 @@ routes.get('/api/:category', (req, res)=>{
     req.getConnection((err, conn)=>{
         if(err) return res.send(err)
         
-        conn.query('SELECT * FROM `e-commerce`.products WHERE category = ?', [req.params.category], (err, rows)=>{
+        conn.query('SELECT * FROM u284866064_coopedatabase.products WHERE category = ?', [req.params.category], (err, rows)=>{
             if(err) return res.send(err)
 
             res.json(rows)
